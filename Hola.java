ceacc9eb00a727a24b9ad0b4fdb18105481052d6
Agustin Sidauy <sidauy23>
2025-09-25 09:06:58 +0200

    Modificar: saludo en java
    Se corrigieron errores, se verifica que se introduce un nombre

--- Hola.java
+++ Hola.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Hola{
 	public static void main(String[] args){
-			system.out.println("Dime tu nombre");
-			Scanner scn = Scanner(System.in);
-			String nombre = scn.nextLine();
-			system.out.println("Hola " + nombre);
-			//lol
+
+			Scanner scn = new Scanner(System.in);
+			Boolean salida = false;
+			
+			while(!salida){
+				System.out.println("Dime tu nombre: ");
+				String nombre = scn.nextLine();
+				if(nombre.length()<1){
+					System.out.println("El nombre no puede estar vacío");
+				} else {
+					System.out.println("Hola " + nombre);
+					salida = true;
+				}
+			}
 	}
 }

2cbd91af13602f8a17e3ca1a416204e33091b49b
Agustin Sidauy <sidauy23>
2025-09-25 08:54:36 +0200

    Modificar: saludo en java
    Se agregó función que pide el nombre al usuario y devuelve saludo personalizado

--- Hola.java
+++ Hola.java
@@ -1,6 +1,9 @@
 public class Hola{
 	public static void main(String[] args){
-			system.out.println("Hola");
+			system.out.println("Dime tu nombre");
+			Scanner scn = Scanner(System.in);
+			String nombre = scn.nextLine();
+			system.out.println("Hola " + nombre);
 			//lol
 	}
 }
